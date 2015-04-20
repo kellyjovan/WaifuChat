48407fd1d48c62f9fbaac9555e7fbbb595b0b13b
github username <github email address>
2015-04-19 20:20:55 -0400

    changed appearance of new character page, added remove feature

--- public/js/script.js
+++ public/js/script.js
@@ -3,13 +3,17 @@ $(document).ready(function(){
       like_btn = $('#like_btn'),
       appearance_btn = $('#appearance_btn');
 
-  dislike_btn.click(function(){
-    $('.dislikes').append('<input type="text" class="form-control" name="dislikes[]" placeholder="Dislike"><br/>');
-  })
   like_btn.click(function(){
-    $('.likes').append('<input type="text" class="form-control" name="likes[]" placeholder="Like"><br/>');
+    $('.likes').append('<div class="form-group"><input type="text" class="form-control" name="likes[]" placeholder="Like"><i class="fa fa-times"></i></div>');
+  });
+  dislike_btn.click(function(){
+    $('.dislikes').append('<div class="form-group"><input type="text" class="form-control" name="dislikes[]" placeholder="Dislike"><i class="fa fa-times"></i></div>');
   });
   appearance_btn.click(function(){
-    $('.appearances').append('<input type="text" class="form-control" name="appearances" placeholder="Appearance"><br/>');
+    $('.appearances').append('<div class="form-group"><input type="text" class="form-control" name="appearances[]" placeholder="Appearance"><i class="fa fa-times"></i></div>');
+  });
+
+  $('.lwrapper').on('click', '.fa-times',function(){
+    $(this).parent('div').remove();
   });
 });
