918a940010bc0eeb4efa8c1af86a0945cd11f729
github username <github email address>
2015-05-06 17:01:26 -0400

    fixed index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,9 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
-    @user = User.find(session[:user_id])
+   if session[:user_id]
+      @user = User.find(session[:user_id])
+    end
     erb :index
   end
 
